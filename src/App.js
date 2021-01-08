f354656c1ed2720b64f3f89a0e0535c86bcdd6a2
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2021-01-08 13:56:25 +0530

    collection preview page or shop page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import logo from './logo.svg';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-
-const HatsPage =() => (
-  <div>
-    <h1>Hatspage</h1>
-  </div>
-)
 
 
 
@@ -21,7 +15,7 @@ function App() {
 
     <Switch>
      <Route exact path ='/' component={HomePage}/>
-     <Route  path='/shop/hats' component={HatsPage}/>
+     <Route exact path='/shop' component={ShopPage}/>
      
      </Switch>
     </div>
