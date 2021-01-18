16544f26d77608b421695c4983268818fc865ff0
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2021-01-18 11:10:41 +0530

    updating our app with reselect memorization

--- src/components/cart-dropdown/cart-dropdown.componnet.jsx
+++ src/components/cart-dropdown/cart-dropdown.componnet.jsx
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
+import {selectCartItems} from '../../redux/cart/cart.selector';
 
 const CartDropDown =({cartItems}) => (
     <div className='cart-dropdown'>
@@ -19,8 +20,8 @@ const CartDropDown =({cartItems}) => (
     </div>
 )
 
-const mapStateToProps =({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps =(state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropDown);
