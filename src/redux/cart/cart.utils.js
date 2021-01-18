80b0ec633b6ace54211ce32efd5e8f9ac78cb8c2
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2021-01-18 12:58:26 +0530

    adding and removing item funcitonality

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,5 +1,6 @@
 
 
+
 export const addItemToCart =(CartItems,cartItemToAdd) => {
     
     const existingCartItem = CartItems.find(CartItem => CartItem.id == cartItemToAdd.id)
@@ -17,4 +18,23 @@ export const addItemToCart =(CartItems,cartItemToAdd) => {
     return [...CartItems,{...cartItemToAdd, quantity: 1}];
     
 
+}
+
+
+export const removeItemFromCart =(cartItems,cartItemToRemove) => {
+
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id )
+    }
+
+    return cartItems.map(
+        cartItem => 
+        cartItem.id === cartItemToRemove.id ?
+        {...cartItem,quantity:cartItem.quantity - 1}
+        : cartItem
+    )
 }
