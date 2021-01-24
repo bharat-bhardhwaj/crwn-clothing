2d298b37e5e50ee739e38cb4a1b3bb36c188ef3e
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2021-01-24 14:02:13 +0530

    use selectors memorize the data using reselect add local storage using reduc pressist

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,33 +1,15 @@
-import React from 'react';
-import SHOP_DATA from './shop.data.js'
+import React from 'react'
+import {Route} from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props)
+import CollectionPage from '../collection/collection.component';
 
-        this.state ={
-            collections:SHOP_DATA
-        }
-    }
-
-    render() {
-
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
-
-                    {
-                        collections.map( ({id, ...otherCollectionProps}) => (
-                            <CollectionPreview key={id} {...otherCollectionProps}/>
-                        ))
-                    }
-
-            </div>
-        )
-    }
-
-}
+const ShopPage = ({match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+    <Route exact path={`${match.path}/:collectionID`} component={CollectionPage}/>
+  </div>
+)
 
 export default ShopPage
